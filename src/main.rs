55c6daffd3c2f529ffabbaed8a200b8f82904a0d
I_Nosovskiy <mananaxep@gmail.com>
2021-04-30 15:42:48 +0300

    change navbar and footer

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+#![recursion_limit="1024"]
 use yew::prelude::*;
+use yew_router::{route::Route, switch::Permissive};
 
-enum Msg {
-    AddOne,
+mod components;
+mod content;
+mod generator;
+mod pages;
+use pages::{
+    author::Author, author_list::AuthorList, home::Home, page_not_found::PageNotFound, post::Post,
+    post_list::PostList, about_us::AboutUs,
+};
+mod switch;
+use switch::{AppAnchor, AppRoute, AppRouter, PublicUrlSwitch};
+
+pub enum Msg {
+    ToggleNavbar,
 }
 
-struct Model {
-    // `ComponentLink` is like a reference to a component.
-    // It can be used to send messages to the component
+pub struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    navbar_active: bool,
 }
-
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
@@ -18,38 +28,186 @@ impl Component for Model {
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            value: 0,
+            navbar_active: false,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
+            Msg::ToggleNavbar => {
+                self.navbar_active = !self.navbar_active;
                 true
             }
         }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
     fn view(&self) -> Html {
         html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <>
+                { self.view_nav() }
+
+                <main>
+                    <AppRouter
+                        render=AppRouter::render(Self::switch)
+                        redirect=AppRouter::redirect(|route: Route| {
+                            AppRoute::PageNotFound(Permissive(Some(route.route))).into_public()
+                        })
+                    />
+                </main>
+                <footer class="footer">
+                    // left footer
+                    <div class="content has-text-left">
+                        <h4>
+                            { "Let’s stay in touch!" }
+                        </h4>
+                        <div class="field">
+                          <p class="control has-icons-left has-icons-right">
+                            <input class="input" type="email" placeholder="mail@gmail.com"/>
+                            <span class="icon is-small is-left">
+                              <i class="fas fa-envelope"></i>
+                            </span>
+                            <span class="icon is-small is-right">
+                              <i class="fas fa-check"></i>
+                            </span>
+                          </p>
+                        </div>
+                        <div class="buttons">
+                          <a class="button">
+                            <strong> { "Eng" } </strong>
+                          </a>
+                          <a class="button is-light">
+                             { "Rus" }
+                          </a>
+                        </div>
+                        <a class=vec!("social-network") href="mailto:info@cadbase.ru" title="Email">
+                            <i class="fas fa-lg fa-envelope"></i>
+                        </a>
+                        <a class=vec!("social-network") href="#" title="Twitter">
+                            <i class="fas fa-lg fa-envelope"></i>
+                        </a>
+                    </div>
+                    // center footer
+                    <div class="content has-text-centered">
+                        <h4> { "About us" } </h4>
+                        <ul>
+                            <li><a href="#">{ "Licence" }</a></li>
+                            <li><a href="#">{ "Corporate Info" }</a></li>
+                            <li><a href="#">{ "Corporate Responsibility" }</a></li>
+                            <li><a href="#">{ "Our Models" }</a></li>
+                            <li><a href="#">{ "Information" }</a></li>
+                        </ul>
+                    </div>
+                    // right footer
+                    <div class="content has-text-right">
+                        <h4> { "Customer services" } </h4>
+                        <ul>
+                            <li><a href="#">{ "FAQ" }</a></li>
+                            <li><a href="#">{ "Contact" }</a></li>
+                            <li><a href="#">{ "Your history" }</a></li>
+                            <li><a href="#">{ "News" }</a></li>
+                            <li><a href="#">{ "Payment" }</a></li>
+                        </ul>
+                    </div>
+                </footer>
+            </>
+        }
+    }
+}
+impl Model {
+    fn view_nav(&self) -> Html {
+        let Self {
+            ref link,
+            navbar_active,
+            ..
+        } = *self;
+
+        let active_class = if navbar_active { "is-active" } else { "" };
+
+        html! {
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <h1 class="navbar-item is-size-3">
+                        <AppAnchor classes="navbar-item" route=AppRoute::Home>
+                            <img src="logo_min.svg" width="112" height="28"/>
+                        </AppAnchor>
+                    </h1>
+
+                    <a role="button"
+                        class=vec!("navbar-burger", "burger", active_class)
+                        aria-label="menu" aria-expanded="false"
+                        onclick=link.callback(|_| Msg::ToggleNavbar)
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                <div class=vec!("navbar-menu", active_class)>
+                    <div class="navbar-start">
+                        <AppAnchor classes="navbar-item" route=AppRoute::PostList>
+                            { "Catalog" }
+                        </AppAnchor>
+
+                        <AppAnchor classes="navbar-item" route=AppRoute::AuthorList>
+                            { "Tenders" }
+                        </AppAnchor>
+
+                        <AppAnchor classes="navbar-item" route=AppRoute::AboutUs>
+                            { "About Us" }
+                        </AppAnchor>
+                    </div>
+                    <div class="navbar-end">
+                      <div class="navbar-item">
+                        <div class="buttons">
+                          <a class="button">
+                            <strong> { "Sign up" } </strong>
+                          </a>
+                          <a class="button is-light">
+                             { "Log in" }
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </nav>
+        }
+    }
+
+    fn switch(switch: PublicUrlSwitch) -> Html {
+        match switch.route() {
+            AppRoute::Post(id) => {
+                html! { <Post seed=id /> }
+            }
+            AppRoute::PostListPage(page) => {
+                html! { <PostList page=page.max(1) /> }
+            }
+            AppRoute::PostList => {
+                html! { <PostList page=1 /> }
+            }
+            AppRoute::Author(id) => {
+                html! { <Author seed=id /> }
+            }
+            AppRoute::AuthorList => {
+                html! { <AuthorList /> }
+            }
+            AppRoute::AboutUs => {
+                html! { <AboutUs /> }
+            }
+            AppRoute::Home => {
+                html! { <Home /> }
+            }
+            AppRoute::PageNotFound(Permissive(route)) => {
+                html! { <PageNotFound route=route /> }
+            }
         }
     }
 }
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
     yew::start_app::<Model>();
 }
---
+++ src/pages/mod.rs
@@ -0,0 +1,7 @@
+pub mod author;
+pub mod author_list;
+pub mod home;
+pub mod page_not_found;
+pub mod post;
+pub mod post_list;
+pub mod about_us;
