b730fa097cedc77c314edc2bcc52cbfcd4a26fc3
mnnxp <mananaxep@gmail.com>
2021-10-22 14:35:19 +0300

    add negative test

--- tests/login_test.js
+++ tests/login_test.js
@@ -4,20 +4,11 @@ const apiPort = process.env.PORT || 8080;
 const apiDomain = process.env.DOMAIN || "0.0.0.0";
 const url = `http://${apiDomain}:${apiPort}`;
 
+const baseUsername = "qwe";
+const basePassword = "qweqwe";
+
 fixture `Check login`// declare the fixture
     .page `${url}/#/login`;  // specify the start page
-
-
-// authentication with correct username/password
-test('Ok authentication', async t => {
-    await t
-        .typeText('#username', 'qwe')
-        .typeText('#password', 'qweqwe')
-        .click('#submit-button')
-
-        // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector('h2').filter('.subtitle').innerText).eql('engineer component supplier');
-});
 
 // authentication with bad username
 test('Unauthorized', async t => {
@@ -31,4 +22,15 @@ test('Unauthorized', async t => {
           .filter('.notification')
           .filter('.is-danger').innerText)
           .eql('Unauthorized');
+});
+
+// authentication with correct username/password
+test('Ok authentication', async t => {
+    await t
+        .typeText('#username', baseUsername)
+        .typeText('#password', basePassword)
+        .click('#submit-button')
+
+        // Use the assertion to check if the actual header text is equal to the expected one
+        .expect(Selector('h2').filter('.subtitle').innerText).eql('engineer component supplier');
 });
