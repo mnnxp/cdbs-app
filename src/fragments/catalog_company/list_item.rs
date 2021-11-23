035d625d7d0a131134477452439abcfa7c12ce99
sansx <646924078@qq.com>
2021-11-24 00:55:12 +0800

    fix update && css bug

--- src/fragments/catalog_company/list_item.rs
+++ src/fragments/catalog_company/list_item.rs
@@ -63,7 +63,7 @@ impl Component for ListItem {
         // Should only return "true" if new properties are different to
         // previously received properties.
         // This company has no properties so we will always return "false".
-        if self.props.show_list != props.show_list || self.props.data.is_followed != props.data.is_followed {
+        if self.props.show_list != props.show_list || self.props.data.is_followed != props.data.is_followed || self.props.data.uuid != props.data.uuid {
             self.props.show_list = props.show_list;
             self.props.data = props.data;
             true
@@ -191,7 +191,7 @@ impl ListItem {
 
         html! {
           <div class="boxItem" >
-            <div class="innerBox flex-box" >
+            <div class="innerBox" >
               <div class="imgBox" >
                 <div class="top-tag" hidden={!is_supplier} >{"supplier"}</div>
                 <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image" />
