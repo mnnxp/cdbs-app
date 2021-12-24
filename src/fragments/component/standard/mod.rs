428e2e7f7893fd7e53e765538dafa9b0e0854f1a
mnnxp <mananaxep@gmail.com>
2021-12-24 22:51:56 +0300

    make add and delete component standards

--- src/fragments/component/standard/mod.rs
+++ src/fragments/component/standard/mod.rs
@@ -3,10 +3,42 @@ mod standard_item;
 pub use standard_item::ComponentStandardItem;
 
 use std::collections::BTreeSet;
+use chrono::NaiveDateTime;
+use yew::prelude::*;
 use yew::{Component, ComponentLink, Html, Properties, ShouldRender, html};
-// use log::debug;
-// use crate::error::{get_error, Error};
+use log::debug;
+use graphql_client::GraphQLQuery;
+use serde_json::Value;
+use wasm_bindgen_futures::spawn_local;
+
+use crate::error::{get_error, Error};
+use crate::fragments::list_errors::ListErrors;
+use crate::gqls::make_query;
 use crate::types::{UUID, ShowStandardShort};
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "./graphql/schema.graphql",
+    query_path = "./graphql/components.graphql",
+    response_derives = "Debug"
+)]
+struct AddStandardToComponent;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "./graphql/schema.graphql",
+    query_path = "./graphql/components.graphql",
+    response_derives = "Debug"
+)]
+struct GetComponentStandards;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "./graphql/schema.graphql",
+    query_path = "./graphql/standards.graphql",
+    response_derives = "Debug"
+)]
+struct GetStandardsShortList;
 
 #[derive(Clone, Debug, Properties)]
 pub struct Props {
@@ -17,14 +49,29 @@ pub struct Props {
 }
 
 pub struct ComponentStandardsCard {
+    error: Option<Error>,
     props: Props,
     link: ComponentLink<Self>,
     standard_uuids: BTreeSet<UUID>,
+    component_standards: Vec<ShowStandardShort>,
+    standard_list: Vec<ShowStandardShort>,
+    request_add_standard_uuid: UUID,
+    hide_add_standard_modal: bool,
 }
 
 #[derive(Clone)]
 pub enum Msg {
     DeleteComponentStandard(UUID),
+    RequestStandardsList,
+    RequestAddStandard,
+    RequestComponentStandards,
+    GetStandardsListResult(String),
+    GetComponentStandardsResult(String),
+    GetAddStandardResult(String),
+    UpdateSelectStandard(String),
+    ChangeHideAddStandard,
+    SetSelectStandard,
+    ClearError,
     Ignore,
 }
 
@@ -39,18 +86,124 @@ impl Component for ComponentStandardsCard {
             standard_uuids.insert(standard.uuid.clone());
         };
 
+        let component_standards = props.component_standards.clone();
+
         Self {
+            error: None,
             props,
             link,
             standard_uuids,
+            component_standards,
+            standard_list: Vec::new(),
+            request_add_standard_uuid: String::new(),
+            hide_add_standard_modal: true,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        let link = self.link.clone();
+
         match msg {
             Msg::DeleteComponentStandard(standard_uuid) => {
                 self.standard_uuids.remove(&standard_uuid);
+                link.send_message(Msg::SetSelectStandard);
             },
+            Msg::RequestStandardsList => {
+                spawn_local(async move {
+                    let res = make_query(GetStandardsShortList::build_query(
+                        get_standards_short_list::Variables { ipt_standards_arg: None }
+                    )).await.unwrap();
+                    link.send_message(Msg::GetStandardsListResult(res));
+                })
+            },
+            Msg::RequestAddStandard => {
+                let ipt_standard_to_component_data = add_standard_to_component::IptStandardToComponentData{
+                    componentUuid: self.props.component_uuid.clone(),
+                    standardUuid: self.request_add_standard_uuid.clone(),
+                };
+                spawn_local(async move {
+                    let res = make_query(AddStandardToComponent::build_query(
+                        add_standard_to_component::Variables { ipt_standard_to_component_data }
+                    )).await.unwrap();
+                    link.send_message(Msg::GetAddStandardResult(res));
+                })
+            },
+            Msg::RequestComponentStandards => {
+                let component_uuid = self.props.component_uuid.clone();
+                spawn_local(async move {
+                    let res = make_query(GetComponentStandards::build_query(
+                        get_component_standards::Variables { component_uuid }
+                    )).await.unwrap();
+                    link.send_message(Msg::GetComponentStandardsResult(res));
+                })
+            },
+            Msg::GetStandardsListResult(res) => {
+                let data: Value = serde_json::from_str(res.as_str()).unwrap();
+                let res_value = data.as_object().unwrap().get("data").unwrap();
+
+                match res_value.is_null() {
+                    false => {
+                        let result: Vec<ShowStandardShort> =
+                            serde_json::from_value(res_value.get("standards").unwrap().clone()).unwrap();
+                        debug!("standards: {:?}", result);
+                        self.standard_list = result;
+                        link.send_message(Msg::SetSelectStandard);
+                    },
+                    true => self.error = Some(get_error(&data)),
+                }
+            },
+            Msg::GetAddStandardResult(res) => {
+                let data: Value = serde_json::from_str(res.as_str()).unwrap();
+                let res_value = data.as_object().unwrap().get("data").unwrap();
+
+                match res_value.is_null() {
+                    false => {
+                        let result: bool =
+                            serde_json::from_value(res_value.get("addStandardToComponent").unwrap().clone()).unwrap();
+                        debug!("addStandardToComponent: {:?}", result);
+                        self.hide_add_standard_modal = result;
+                        link.send_message(Msg::RequestComponentStandards);
+                    },
+                    true => self.error = Some(get_error(&data)),
+                }
+            },
+            Msg::GetComponentStandardsResult(res) => {
+                let data: Value = serde_json::from_str(res.as_str()).unwrap();
+                let res_value = data.as_object().unwrap().get("data").unwrap();
+
+                match res_value.is_null() {
+                    false => {
+                        let result: Vec<ShowStandardShort> =
+                            serde_json::from_value(res_value.get("component").unwrap()
+                                .get("componentStandards").unwrap().clone()).unwrap();
+                        debug!("componentStandards: {:?}", result);
+                        self.component_standards = result;
+                        self.standard_uuids = BTreeSet::new();
+                        for standard in self.component_standards.clone() {
+                            self.standard_uuids.insert(standard.uuid.clone());
+                        };
+                        link.send_message(Msg::SetSelectStandard);
+                    },
+                    true => self.error = Some(get_error(&data)),
+                }
+            },
+            Msg::UpdateSelectStandard(data) => self.request_add_standard_uuid = data,
+            Msg::ChangeHideAddStandard => {
+                if self.hide_add_standard_modal && self.standard_list.is_empty() {
+                    link.send_message(Msg::RequestStandardsList)
+                }
+                self.hide_add_standard_modal = !self.hide_add_standard_modal
+            },
+            Msg::SetSelectStandard => {
+                self.request_add_standard_uuid = String::new();
+                for standard in self.standard_list.iter() {
+                    if let None = self.standard_uuids.get(&standard.uuid) {
+                        self.request_add_standard_uuid = standard.uuid.clone();
+                        break;
+                    }
+                }
+            },
+            Msg::ClearError => self.error = None,
             Msg::Ignore => {},
         }
         true
@@ -58,43 +211,133 @@ impl Component for ComponentStandardsCard {
 
     fn change(&mut self, props: Self::Properties) -> ShouldRender {
         if self.props.component_uuid == props.component_uuid &&
-                self.props.component_standards.len() == props.component_standards.len() {
+             self.props.component_standards.len() == props.component_standards.len() {
             false
         } else {
+            self.standard_uuids = BTreeSet::new();
+            for standards in props.component_standards.iter() {
+                self.standard_uuids.insert(standards.uuid.clone());
+            };
+            self.hide_add_standard_modal = true;
             self.props = props;
             true
         }
     }
 
     fn view(&self) -> Html {
+        let onclick_clear_error = self.link.callback(|_| Msg::ClearError);
+
+        html!{<>
+            <ListErrors error=self.error.clone() clear_error=Some(onclick_clear_error.clone())/>
+            {self.show_standards()}
+        </>}
+    }
+}
+
+impl ComponentStandardsCard {
+    fn show_standards(&self) -> Html {
         let onclick_delete_standard = self.link
             .callback(|value: UUID| Msg::DeleteComponentStandard(value));
 
-        html! {
-            html!{<div class="card">
-              <table class="table is-fullwidth">
-                <tbody>
-                   <th>{"Classifier"}</th>
-                   <th>{"Specified tolerance"}</th>
-                   <th>{"Action"}</th>
-                   {match self.props.show_delete_btn {
-                       true => html!{<th>{"Delete"}</th>},
-                       false => html!{},
-                   }}
-                   {for self.props.component_standards.iter().map(|data| {
-                       match self.standard_uuids.get(&data.uuid) {
-                           Some(_) => html!{<ComponentStandardItem
-                               show_delete_btn = self.props.show_delete_btn
-                               component_uuid = self.props.component_uuid.clone()
-                               standard_data = data.clone()
-                               delete_standard = Some(onclick_delete_standard.clone())
-                             />},
-                           None => html!{},
-                       }
-                   })}
-                </tbody>
-              </table>
-            </div>}
+        let onclick_action_btn = self.link
+            .callback(|_| Msg::ChangeHideAddStandard);
+
+        html!{<div class="card">
+          <table class="table is-fullwidth">
+            <tbody>
+               <th>{"Classifier"}</th>
+               <th>{"Specified tolerance"}</th>
+               <th>{"Action"}</th>
+               {match self.props.show_delete_btn {
+                   true => html!{<th>{"Delete"}</th>},
+                   false => html!{},
+               }}
+               {for self.component_standards.iter().map(|data| {
+                   match self.standard_uuids.get(&data.uuid) {
+                       Some(_) => html!{<ComponentStandardItem
+                           show_delete_btn = self.props.show_delete_btn
+                           component_uuid = self.props.component_uuid.clone()
+                           standard_data = data.clone()
+                           delete_standard = Some(onclick_delete_standard.clone())
+                         />},
+                       None => html!{},
+                   }
+               })}
+            </tbody>
+          </table>
+          {self.modal_add_standard()}
+          <button
+              id="add-standard-component"
+              class="button is-fullwidth"
+              onclick={onclick_action_btn} >
+              // {"Supplier"}
+              <span class="icon" >
+                <i class="fa fa-plus" aria-hidden="true"></i>
+              </span>
+          </button>
+        </div>}
+    }
+
+    fn modal_add_standard(&self) -> Html {
+        let onclick_add_standard = self.link
+            .callback(|_| Msg::RequestAddStandard);
+
+        let onclick_hide_modal = self.link
+            .callback(|_| Msg::ChangeHideAddStandard);
+
+        let onchange_select_add_standard = self.link
+            .callback(|ev: ChangeData| Msg::UpdateSelectStandard(match ev {
+              ChangeData::Select(el) => el.value(),
+              _ => String::new(),
+          }));
+
+        let class_modal = match &self.hide_add_standard_modal {
+            true => "modal",
+            false => "modal is-active",
+        };
+
+        html!{
+            <div class=class_modal>
+              <div class="modal-background" onclick=onclick_hide_modal.clone() />
+                <div class="modal-content">
+                  <div class="card">
+                    <header class="modal-card-head">
+                      <p class="modal-card-title">{"Add standard for component"}</p>
+                      <button class="delete" aria-label="close" onclick=onclick_hide_modal.clone() />
+                    </header>
+                    <section class="modal-card-body">
+                        <label class="label">{"Select standard"}</label>
+                        <div class="select">
+                          <select
+                              id="add-standard"
+                              onchange=onchange_select_add_standard
+                            >
+                          { for self.standard_list.iter().map(|x|
+                              match self.standard_uuids.get(&x.uuid) {
+                                  Some(_) => html!{}, // this standard already has
+                                  None => match self.request_add_standard_uuid == x.uuid {
+                                      true => html!{ <option value={x.uuid.to_string()} selected=true>{
+                                          format!("{} ({})", &x.classifier, &x.name)
+                                      }</option> },
+                                      false => html!{ <option value={x.uuid.to_string()}>{
+                                          format!("{} ({})", &x.classifier, &x.name)
+                                      }</option> },
+                                  },
+                              }
+                          )}
+                          </select>
+                        </div>
+                    </section>
+                    <button
+                        id="standard-component"
+                        class="button"
+                        disabled={self.request_add_standard_uuid.is_empty()}
+                        onclick={onclick_add_standard} >
+                        {"Add"}
+                    </button>
+                  </div>
+                </div>
+              </div>
         }
     }
 }
