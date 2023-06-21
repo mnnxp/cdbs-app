7603ffe9b22852aa9e0777ef94db23b434cd8c60
mnnxp <mananaxep@gmail.com>
2023-06-21 22:28:46 +0300

    fix warnings

--- src/fragments/standard/mod.rs
+++ src/fragments/standard/mod.rs
@@ -4,8 +4,8 @@ mod keyword;
 mod spec;
 mod update_favicon;
 
-pub use catalog::*;
-pub use file::*;
-pub use keyword::*;
-pub use spec::*;
-pub use update_favicon::*;
+pub use catalog::{CatalogStandards, ListItemStandard};
+pub use file::{FileItem, StandardFilesCard};
+pub use keyword::{KeywordsTags, KeywordTagItem, AddKeywordsTags};
+pub use spec::{SpecsTags, SpecTagItem, SearchSpecsTags};
+pub use update_favicon::UpdateStandardFaviconCard;
