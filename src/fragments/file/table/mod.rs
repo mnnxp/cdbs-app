dd1d1d3307a1f75ded85642704aa69abd5f2949b
mnnxp <mananaxep@gmail.com>
2023-08-29 18:42:33 +0300

    add short name of the revision field for table

--- src/fragments/file/table/mod.rs
+++ src/fragments/file/table/mod.rs
@@ -7,7 +7,6 @@ use crate::services::get_value_field;
 
 pub struct FileHeadersShow {
   props: Props,
-  // link: ComponentLink<Self>,
 }
 
 #[derive(Properties, Clone)]
@@ -22,10 +21,7 @@ impl Component for FileHeadersShow {
     type Properties = Props;
 
     fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-      FileHeadersShow {
-        props,
-        // link,
-      }
+      FileHeadersShow { props }
     }
 
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
@@ -54,15 +50,15 @@ impl FileHeadersShow {
       html!{
         <thead>
           <tr>
-            <th>{ get_value_field(&120) }</th> // Filename
-            <th>{ get_value_field(&308) }</th> // Revision
-            // <th>{ get_value_field(&121) }</th> // Content
-            <th>{ get_value_field(&122) }</th> // Filesize
-            // <th>{ get_value_field(&26) }</th> // Program
-            <th>{ get_value_field(&124) }</th> // Upload by
-            <th>{ get_value_field(&125) }</th> // Upload at
+            <th>{get_value_field(&120)}</th> // Filename
+            <th><abbr title={get_value_field(&308)}>{get_value_field(&309)}</abbr></th> // Revision
+            // <th>{get_value_field(&121)}</th> // Content
+            <th>{get_value_field(&122)}</th> // Filesize
+            // <th>{get_value_field(&26) </th> // Program
+            <th>{get_value_field(&124)}</th> // Upload by
+            <th>{get_value_field(&125)}</th> // Upload at
             {match &self.props.show_download_btn {
-                true => html!{<th>{ get_value_field(&126) }</th>}, // Download
+                true => html!{<th>{get_value_field(&126)}</th>}, // Download
                 false => html!{},
             }}
           </tr>
@@ -74,9 +70,9 @@ impl FileHeadersShow {
       html!{
         <thead>
           <tr>
-            <th>{ get_value_field(&120) }</th> // Filename
-            <th>{ get_value_field(&122) }</th> // Filesize
-            <th>{ get_value_field(&126) }</th> // Download
+            <th>{get_value_field(&120)}</th> // Filename
+            <th>{get_value_field(&122)}</th> // Filesize
+            <th>{get_value_field(&126)}</th> // Download
           </tr>
         </thead>
       }
