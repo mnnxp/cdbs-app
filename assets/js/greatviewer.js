500901b7ec67494a902038b2f190a968c0f30d54
mnnxp <mananaxep@gmail.com>
2023-12-29 23:53:06 +0300

    rm cdn and change paths for three

--- assets/js/greatviewer.js
+++ assets/js/greatviewer.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three'
-import { STLLoader } from 'three/addons/loaders/STLLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import Stats from 'three/addons/libs/stats.module.js';
-import { GUI } from 'lil-gui';
+import * as THREE from '../../../../cdn/three/three.module.js';
+import { STLLoader } from '../../../../cdn/three/STLLoader.js';
+import { OrbitControls } from '../../../../cdn/three/OrbitControls.js';
+import Stats from '../../../../cdn/three/stats.module.js';
+import { GUI } from '../../../../cdn/three/lil-gui.js';
 
 export class GreatViewer {
     constructor(model_path, size_flag, i18n_str) {
