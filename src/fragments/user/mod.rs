6b0cd720ba3c6251a75b597c51eef0601e5a4981
mnnxp <mananaxep@gmail.com>
2024-06-21 23:45:18 +0300

    add user-owner preview

--- src/fragments/user/mod.rs
+++ src/fragments/user/mod.rs
@@ -1,5 +1,7 @@
 mod catalog;
 mod certificate;
+mod modal_card;
 
 pub use catalog::{CatalogUsers, ListItemUser};
 pub use certificate::{UserCertificatesCard, UserCertificateItem, AddUserCertificateCard};
+pub use modal_card::ModalCardUser;
