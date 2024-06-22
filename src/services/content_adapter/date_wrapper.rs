c697cfbd49dd24a898172dbfed9a8df9277be304
mnnxp <mananaxep@gmail.com>
2024-06-22 21:00:23 +0300

    change tag to time for date

--- src/services/content_adapter/date_wrapper.rs
+++ src/services/content_adapter/date_wrapper.rs
@@ -3,42 +3,58 @@ use chrono::NaiveDateTime;
 use yew::{html, Html};
 use crate::services::{get_lang, get_value_field};
 
+/// Contains three time formats
+/// to display in different attributes of the time tag
+struct StrFmtDate<'a> {
+    title: StrftimeItems<'a>,
+    datetime: StrftimeItems<'a>,
+    text: StrftimeItems<'a>,
+}
+
 /// Returns Html code with two dates in abbr tag
 pub(crate) fn two_dates_display(created_dt: &NaiveDateTime, updated_dt: &NaiveDateTime) -> Html {
-    let lang = get_lang().unwrap_or(String::new());
-    let (fmt_dt, fmt_d) = match lang.as_str() {
-        "ru" => (StrftimeItems::new("%d.%m.%Y %H:%M:%S"), StrftimeItems::new("%d.%m.%Y")),
-        _ => (StrftimeItems::new("%Y-%m-%d %H:%M:%S"), StrftimeItems::new("%Y-%m-%d")),
-    };
-
+    let srtfmt = srtfmt_dt();
     html!{
-        <abbr title = {format!(
+        <time
+        title={format!(
             "{} {}\n{} {}",
             get_value_field(&276),
-            created_dt.format_with_items(fmt_dt.clone()),
+            created_dt.format_with_items(srtfmt.title.clone()),
             get_value_field(&277),
-            updated_dt.format_with_items(fmt_dt)
-        )}>
+            updated_dt.format_with_items(srtfmt.title)
+        )}
+        datetime={updated_dt.format_with_items(srtfmt.datetime).to_string()}>
             {format!(
                 "{} {}",
                 get_value_field(&277),
-                updated_dt.format_with_items(fmt_d)
+                updated_dt.format_with_items(srtfmt.text)
             )}
-        </abbr>
+        </time>
     }
 }
 
-/// Returns VNode (Html) with date, with date and time information in addr tag
+/// Returns VNode (Html) with date, with date and time information in time tag
 pub(crate) fn date_display(dt: &NaiveDateTime) -> Html {
-    let lang = get_lang().unwrap_or(String::new());
-    let (fmt_dt, fmt_d) = match lang.as_str() {
-        "ru" => (StrftimeItems::new("%d.%m.%Y %H:%M:%S"), StrftimeItems::new("%d.%m.%Y")),
-        _ => (StrftimeItems::new("%Y-%m-%d %H:%M:%S"), StrftimeItems::new("%Y-%m-%d")),
-    };
-
+    let srtfmt = srtfmt_dt();
     html!{
-        <abbr title = {dt.format_with_items(fmt_dt).to_string()}>
-            {dt.format_with_items(fmt_d).to_string()}
-        </abbr>
+        <time
+        title={dt.format_with_items(srtfmt.title).to_string()}
+        datetime={dt.format_with_items(srtfmt.datetime).to_string()}>
+            {dt.format_with_items(srtfmt.text).to_string()}
+        </time>
+    }
+}
+
+/// Sets the time format depending on the language
+fn srtfmt_dt<'a>() -> StrFmtDate<'a> {
+    let datetime = StrftimeItems::new("%Y-%m-%d");
+    let (title, text) = match get_lang().unwrap_or(String::new()).as_str() {
+        "ru" => (StrftimeItems::new("%d.%m.%Y %H:%M:%S"), StrftimeItems::new("%d.%m.%Y")),
+        _ => (StrftimeItems::new("%Y-%m-%d %H:%M:%S"), datetime.clone()),
+    };
+    StrFmtDate{
+        title,
+        datetime,
+        text
     }
 }
