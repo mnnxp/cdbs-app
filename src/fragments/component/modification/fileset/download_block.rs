68845c14f0e448cce67a18ca8604f2cf2c7a3649
mnnxp <mananaxep@gmail.com>
2024-12-17 00:14:50 +0300

    remove useless Download btn

--- src/fragments/component/modification/fileset/download_block.rs
+++ src/fragments/component/modification/fileset/download_block.rs
@@ -1,16 +1,7 @@
 use yew::{Component, Callback, ComponentLink, Html, Properties, ShouldRender, html, ChangeData};
 use log::debug;
-use graphql_client::GraphQLQuery;
-use wasm_bindgen_futures::spawn_local;
-
-use crate::error::Error;
-use crate::fragments::list_errors::ListErrors;
-use crate::fragments::file::FileHeadersShow;
-use crate::fragments::buttons::ft_download_btn;
-use crate::types::{UUID, DownloadFile, FilesetProgramInfo};
-use crate::services::{Size, get_value_field, resp_parsing};
-use crate::gqls::make_query;
-use crate::gqls::component::{ComModFilesetFiles, com_mod_fileset_files};
+use crate::types::{UUID, FilesetProgramInfo};
+use crate::services::get_value_field;
 
 #[derive(Clone, Debug, Properties)]
 pub struct Props {
@@ -21,26 +12,16 @@ pub struct Props {
 }
 
 pub struct ManageFilesOfFilesetBlock {
-    error: Option<Error>,
     props: Props,
     link: ComponentLink<Self>,
     select_fileset_uuid: UUID,
     open_fileset_files_card: bool,
-    open_modal_download_files: bool,
-    file_arr: Vec<DownloadFile>,
-    flag_get_dowload_url: bool,
-    active_loading_files_btn: bool,
 }
 
 pub enum Msg {
-    RequestDownloadFilesetFiles,
-    ResponseError(Error),
-    GetDownloadFilesetFilesResult(String),
     ParseFirstFilesetUuid,
     SelectFilesetUuid(UUID),
-    ShowModalDownloadFiles,
     OpenFilesetFilesBlock,
-    ClearError,
 }
 
 impl Component for ManageFilesOfFilesetBlock {
@@ -49,53 +30,15 @@ impl Component for ManageFilesOfFilesetBlock {
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
-            error: None,
             props,
             link,
             select_fileset_uuid: String::new(),
             open_fileset_files_card: false,
-            open_modal_download_files: false,
-            file_arr: Vec::new(),
-            flag_get_dowload_url: false,
-            active_loading_files_btn: false,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        let link = self.link.clone();
-
         match msg {
-            Msg::RequestDownloadFilesetFiles => {
-                debug!("Select fileset: {:?}", self.select_fileset_uuid);
-                if self.select_fileset_uuid.len() == 36 {
-                    // set active loading button
-                    self.active_loading_files_btn = true;
-                    let ipt_file_of_fileset_arg = com_mod_fileset_files::IptFileOfFilesetArg{
-                        filesetUuid: self.select_fileset_uuid.clone(),
-                        fileUuids: None,
-                    };
-                    spawn_local(async move {
-                        let res = make_query(ComModFilesetFiles::build_query(com_mod_fileset_files::Variables {
-                            ipt_file_of_fileset_arg
-                        })).await.unwrap();
-
-                        link.send_message(Msg::GetDownloadFilesetFilesResult(res));
-                    })
-                }
-            },
-            Msg::ResponseError(err) => self.error = Some(err),
-            Msg::GetDownloadFilesetFilesResult(res) => {
-                match resp_parsing(res, "componentModificationFilesetFiles") {
-                    Ok(result) => {
-                        self.file_arr = result;
-                        debug!("componentModificationFilesetFiles: {:?}", self.file_arr);
-                        self.flag_get_dowload_url = true;
-                        self.open_modal_download_files = true;
-                        self.active_loading_files_btn = false;
-                    },
-                    Err(err) => link.send_message(Msg::ResponseError(err)),
-                }
-            },
             Msg::ParseFirstFilesetUuid => {
                 self.select_fileset_uuid = self.props.current_filesets_program
                     .first()
@@ -110,15 +53,11 @@ impl Component for ManageFilesOfFilesetBlock {
             Msg::SelectFilesetUuid(fileset_uuid) => {
                 self.props.callback_select_fileset_uuid.emit(fileset_uuid.clone());
                 self.select_fileset_uuid = fileset_uuid;
-                // for get new download urls
-                self.flag_get_dowload_url = false;
             },
-            Msg::ShowModalDownloadFiles => self.open_modal_download_files = !self.open_modal_download_files,
             Msg::OpenFilesetFilesBlock => {
                 self.open_fileset_files_card = !self.open_fileset_files_card;
                 self.props.callback_open_fileset.emit(self.open_fileset_files_card);
             },
-            Msg::ClearError => self.error = None,
         }
         true
     }
@@ -137,18 +76,6 @@ impl Component for ManageFilesOfFilesetBlock {
     }
 
     fn view(&self) -> Html {
-        let onclick_clear_error = self.link.callback(|_| Msg::ClearError);
-
-        html!{<>
-            <ListErrors error={self.error.clone()} clear_error={onclick_clear_error.clone()}/>
-            {self.modal_download_files()}
-            {self.show_download_block()}
-        </>}
-    }
-}
-
-impl ManageFilesOfFilesetBlock {
-    fn show_download_block(&self) -> Html {
         let onchange_select_fileset_btn =
             self.link.callback(|ev: ChangeData| Msg::SelectFilesetUuid(match ev {
                 ChangeData::Select(el) => el.value(),
@@ -156,103 +83,36 @@ impl ManageFilesOfFilesetBlock {
             }));
         let onclick_open_fileset_files_list_btn =
             self.link.callback(|_| Msg::OpenFilesetFilesBlock);
-        let onclick_download_fileset_btn = match self.flag_get_dowload_url {
-            true => self.link.callback(|_| Msg::ShowModalDownloadFiles),
-            false => self.link.callback(|_| Msg::RequestDownloadFilesetFiles),
-        };
         let class_fileset_btn = match self.open_fileset_files_card {
             true => "button is-light is-active",
             false => "button",
         };
-        let class_download_btn = match self.active_loading_files_btn {
-            true => "button is-info is-active is-loading",
-            false => "button is-info",
-        };
-
-        html!{<div style="margin-right: .5rem">
-            <div class="select" style="margin-right: .5rem">
-                <select
-                id={"select-fileset-program-download"}
-                select={self.select_fileset_uuid.clone()}
-                onchange={onchange_select_fileset_btn}
-                title={get_value_field(&207)}>
-                  {for self.props.current_filesets_program.iter().map(|fd|
-                      html!{
-                          <option value={fd.uuid.to_string()}
-                                selected={fd.uuid == self.select_fileset_uuid} >
-                              {fd.program.name.clone()}
-                          </option>
-                      }
-                  )}
-                </select>
-            </div>
-            <button
-            class={class_fileset_btn}
-            // disabled = self.select_fileset_uuid.len() != 36
-            onclick={onclick_open_fileset_files_list_btn}
-            title={get_value_field(&106)}>
-                <span class="icon is-small"><i class="fa fa-list"></i></span>
-            </button>
-            <button
-            class={class_download_btn}
-            disabled={self.select_fileset_uuid.len() != 36}
-            onclick={onclick_download_fileset_btn}
-            title={get_value_field(&126)}>
-              <span class="has-text-weight-bold">{get_value_field(&126)}</span>
-            </button>
-        </div>}
-    }
-
-    fn modal_download_files(&self) -> Html {
-        let onclick_modal_download_btn =
-            self.link.callback(|_| Msg::ShowModalDownloadFiles);
-        let class_modal = match &self.open_modal_download_files {
-            true => "modal is-active",
-            false => "modal",
-        };
-        let mut fileset_name = "";
-        for fd in self.props.current_filesets_program.iter() {
-            if fd.uuid == self.select_fileset_uuid {
-                fileset_name = &fd.program.name;
-                break;
-            }
-        }
-
-        html!{<div class={class_modal}>
-          <div class="modal-background" onclick={onclick_modal_download_btn.clone()} />
-            <div class="card">
-              <div class="modal-content">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">
-                        {get_value_field(&138)}
-                        {fileset_name}
-                    </p>
-                    <button class="delete" aria-label="close" onclick={onclick_modal_download_btn.clone()} />
-                </header>
-                <div class="box itemBox">
-                  <article class="media center-media">
-                      <div class="media-content">
-                        <table class="table is-fullwidth is-striped">
-                          <FileHeadersShow show_long={false} />
-                          <tbody>
-                            {for self.file_arr.iter().map(|file| self.show_item_data(&file))}
-                          </tbody>
-                        </table>
-                      </div>
-                  </article>
+        html!{
+            <div style="margin-right: .5rem">
+                <div class="select" style="margin-right: .5rem">
+                    <select
+                    id={"select-fileset-program-download"}
+                    select={self.select_fileset_uuid.clone()}
+                    onchange={onchange_select_fileset_btn}
+                    title={get_value_field(&207)}>
+                        {for self.props.current_filesets_program.iter().map(|fd|
+                            html!{
+                                <option value={fd.uuid.to_string()}
+                                        selected={fd.uuid == self.select_fileset_uuid} >
+                                    {fd.program.name.clone()}
+                                </option>
+                            }
+                        )}
+                    </select>
                 </div>
-              </div>
-          </div>
-        </div>}
-    }
-
-    fn show_item_data(&self, file_down: &DownloadFile) -> Html {
-      html!{
-        <tr>
-          <td>{file_down.filename.clone()}</td>
-          <td>{file_down.show_size()}</td>
-          <td>{ft_download_btn(file_down.download_url.clone(), false)}</td>
-        </tr>
-      }
+                <button
+                class={class_fileset_btn}
+                onclick={onclick_open_fileset_files_list_btn}
+                title={get_value_field(&106)}>
+                    <span class="icon is-small"><i class="fa fa-list"></i></span>
+                    <span>{get_value_field(&198)}</span>
+                </button>
+            </div>
+        }
     }
 }
