851861245b6d747d184bfeaacef953253261f5ae
mnnxp <mananaxep@gmail.com>
2026-05-14 22:37:18 +0300

    update notification with progress bar

--- src/fragments/notification.rs
+++ src/fragments/notification.rs
@@ -1,15 +1,14 @@
 use yew::{html, Html, classes};
 
 /// Returns a notification with the specified text and color (style)
+/// Disappears automatically after 3 seconds with progress bar
 pub fn show_notification(text: &str, style: &str, is_show: bool) -> Html {
-    let class_notif = classes!("notification", "custom-notif", "hide-after", style.to_string());
-
-    match is_show {
-        true => html!{
-            <div class={class_notif}>
-                {text} // Data updated
-            </div>
-        },
-        false => html!{},
+    if !is_show {
+        return html!{}
+    }
+    html!{
+        <div class={classes!("notification", "p-3", "custom-notif", "hide-after", style.to_string())}>
+            {text}
+        </div>
     }
 }
